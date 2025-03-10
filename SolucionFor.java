022e96b1ad720ef54e44670501c2297560659468
danielugladiator <72814696+danielugladiator@users.noreply.github.com>
2025-03-10 17:33:52 +0100

    Add files via upload

---
+++ SolucionFor.java
@@ -0,0 +1,11 @@
+package Bloque3;
+
+public class SolucionFor {
+    public static void main(String[] args) {
+        System.out.println("Números pares menores que 20:");
+        for (int i = 2; i < 20; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
---
+++ SolucionWhile.java
@@ -0,0 +1,12 @@
+package Bloque3;
+
+public class SolucionWhile {
+    public static void main(String[] args) {
+        int i = 2;
+        System.out.println("Números pares menores que 20:");
+        while (i < 20) {
+            System.out.print(i + " ");
+            i += 2;
+        }
+    }
+}
